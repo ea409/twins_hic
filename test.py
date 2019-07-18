dfc0e32dbcb1d51d4e30380c991c011a209233ad
ealjibur <e.aljibury@lms.mrc.ac.uk>
2019-07-18 10:46:27 +0100

    moving things around

---
+++ test.py
@@ -0,0 +1,35 @@
+import torch 
+from torch.utils.data import Dataset, DataLoader, RandomSampler
+import sys
+import os
+from scipy.sparse import csr_matrix
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+import torch.nn.functional as F
+
+model = torch.load('model.ckpt')
+
+# Test the model
+model.eval()  # eval mode (batchnorm uses moving mean/variance instead of mini-batch mean/variance)
+with torch.no_grad():
+    correct = 0
+    total = 0
+    for i, raw_imgs in enumerate(dataloader):
+        imgs = Variable(raw_imgs['image'].type(torch.FloatTensor))
+        str_labels = np.asarray(raw_imgs['type'])
+        labels = []
+        for label in str_labels:
+            if label == 'WT':
+                labels.append(0)
+            elif label == 'CTCFKO':
+                labels.append(1)
+            else: 
+                labels.append(2)
+        labels = np.asarray(labels)
+        labels = torch.from_numpy(labels)
+        labels = labels.to(device)
+        outputs = model(imgs)
+        _, predicted = torch.max(outputs.data, 1)
+        correct += (predicted == labels).sum().item()
+    print('Test Accuracy of the model on the 10000 test images: {} %'.format(100 * correct / total))
