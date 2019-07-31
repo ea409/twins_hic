be692c6fc4340e7bed2fbfa9f06db62853c2728c
ealjibur <e.aljibury@lms.mrc.ac.uk>
2019-07-31 17:35:08 +0100

    fixed and working

--- cnn_main.py
+++ cnn_main.py
@@ -10,15 +10,16 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.autograd import Variable
 import torch.nn.functional as F
+from torchvision import transforms
 
-resolution = 880000
-split_res = 8
+resolution = 440000
+split_res = 4
 data_res = 5000
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 class HiCDataset(Dataset):
     """Hi-C dataset."""
-    def __init__(self, root_dir, metadata, data_res, resolution, split_res):
+    def __init__(self, root_dir, metadata, data_res, resolution, split_res, transform):
         """
         Args:
             root_dir (string): Directory with all the images + * if it all images in dir.
@@ -33,6 +34,7 @@ class HiCDataset(Dataset):
         self.split_res = split_res 
         self.pixel_size = int(resolution/data_res)
         self.sub_res = int(resolution/split_res)
+        self.transform = transform 
     def mutate_metadata(self):
         metadata=self.metadata
         metadata['classification']=1
@@ -56,14 +58,19 @@ class HiCDataset(Dataset):
         image.x =  (image.x - minmet)/data_res
         image.y =  (image.y - minmet)/data_res
         image_scp = csr_matrix( (image.vals, (image.x.map(int), image.y.map(int)) ), shape=(self.pixel_size,self.pixel_size) ).toarray()
-        image_scp = 100000*(image_scp+np.transpose(image_scp))
+        image_scp = np.log(image_scp+np.transpose(image_scp)+1)/np.max(np.log(image_scp+1))
         img = np.expand_dims(image_scp, axis=0)
-        sample = (torch.Tensor(img), int(metobj.classification.tolist()[0]) )
+        img =torch.Tensor(img)
+        transform=self.transform
+        sample = (transform(img), int(metobj.classification.tolist()[0]) )
         return sample
 
+transform = transforms.Compose([transforms.ToPILImage(),  transforms.ToTensor()])#,  transforms.Normalize([0.5], [0.5])])
 metadata= pd.read_csv("cleaned_data_metadata.csv")
-dataset=HiCDataset("cleaned_data", metadata, data_res, resolution, split_res)
+dataset=HiCDataset("cleaned_data", metadata, data_res, resolution, split_res, transform=transform)
 sampler= RandomSampler(dataset,replacement=False)
+
+
 batch_size=8
 
 dataloader = DataLoader(dataset, batch_size=batch_size, sampler = sampler)
@@ -79,19 +86,19 @@ class Net(nn.Module):
         self.conv1 = nn.Conv2d(1, 20, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(20, 16, 5)
-        self.fc1 = nn.Linear( 4 * 82 * 82, 120)
-        #self.fc1 = nn.Linear( 176 * 176, 120)
+        self.fc1 = nn.Linear(88*88, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, num_classes)
-        self.norm = nn.Softmin(dim=1)
+        #self.norm = nn.Softmin(dim=1)
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 4 * 82 * 82)
+        #x = self.pool(self.conv2(x))
+        #x = self.pool(self.conv1(x))
+        #x = x.view(-1, 4 * 82 * 82)
+        x = x.view(-1, 88 * 88)
         x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
+        x = self.fc2(x)
         x = self.fc3(x)
-        x = self.norm(x)
+        #x = self.norm(x)
         return x
 
 #model = ConvNet(num_classes).to(device)
@@ -99,25 +106,25 @@ model=Net(num_classes)
 
 # Loss and optimizer
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+
+optimizer = optim.Adam(model.parameters())
 
 #  Training
 for epoch in range(20):
     running_loss=0.0
     for i, data in enumerate(dataloader):
-        imgs, labels = data
-        imgs = Variable(imgs.type(torch.FloatTensor))
-        labels=labels.type(torch.LongTensor)
+        inputs, labels = data
         #imgs, labels = data[0].to(device), data[1].to(device)
         # zero gradients 
         optimizer.zero_grad()
-        # forward pass 
-        outputs = model(imgs)
+        outputs = model(inputs)
         loss = criterion(outputs, labels)
-        # backward pass 
         loss.backward()
         optimizer.step()
         running_loss += loss.item()
+        if (i+1) % 2000==0:
+            _, predicted = torch.max(F.softmax(outputs).data, 1)
+            print(loss.item(), sum(predicted ==labels))
         if (i+1) % no_of_batches == 0:
             print ('Epoch [{}/{}], Loss: {:.4f}'
             .format(epoch+1, i, running_loss/no_of_batches))
--- test.py
+++ test.py
@@ -8,9 +8,34 @@ import torch.optim as optim
 from torch.autograd import Variable
 import cnn_main
 import torch.nn.functional as F
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import ImageGrid
 
-model = cnn_main.ConvNet(num_classes)
+model = cnn_main.Net(num_classes)
 model.load_state_dict(torch.load('model.ckpt'))
+
+transform = transforms.Compose([transforms.ToPILImage(),  transforms.ToTensor()])#,  transforms.Normalize([0.5], [0.5])])
+metadata= pd.read_csv("cleaned_data_metadata.csv")
+dataset= cnn_main.HiCDataset("cleaned_data", metadata, data_res, resolution, split_res, transform=transform)
+sampler= RandomSampler(dataset,replacement=False)
+dataloader = DataLoader(dataset, batch_size=len(dataset), sampler = sampler)
+
+def prediction_plot(truth, pred, dataset):
+    bools = ( (labels ==truth) & (predicted ==pred))
+    indices = torch.nonzero(bools)
+    rand = torch.randperm(len(indices))
+    rand = rand[:20]
+    fig = plot.figure(1, (20, 20))
+    grid = ImageGrid(fig, 111,  # similar to subplot(111)
+                    nrows_ncols=(4, 5),  # creates 2x2 grid of axes
+                    axes_pad=0.1,  # pad between axes in inch.
+                    )
+    for i, ind in enumerate(indices[rand]): 
+        img, lab = dataset[int(ind)]
+        print(i)
+        grid[i].imshow(np.array(img)[0])
+    plot.show()
+    return rand
 
 # Test the model
  # eval mode (batchnorm uses moving mean/variance instead of mini-batch mean/variance)
@@ -18,13 +43,23 @@ with model.eval():
     correct = 0
     total = 0
     for i, data in enumerate(dataloader):
-        imgs, labels = data
-        imgs = Variable(imgs.type(torch.FloatTensor))
-        labels = np.asarray(labels)
-        labels = torch.from_numpy(labels)
-        labels = labels.to(device)
-        outputs = model(imgs)
+        inputs, labels = data
+        outputs = F.softmax(model(inputs))
         total += len(labels)
         _, predicted = torch.max(outputs.data, 1)
         correct += (predicted == labels).sum().item()
-    print('Test Accuracy of the model test images: {} %'.format(100 * correct / total))
+
+
+prediction_plot(0, 0, dataset)
+prediction_plot(0, 1, dataset)
+prediction_plot(0, 2, dataset)
+prediction_plot(1, 0, dataset)
+prediction_plot(1, 1, dataset)
+prediction_plot(1, 2, dataset)
+prediction_plot(2, 0, dataset)
+prediction_plot(2, 1, dataset)
+prediction_plot(2, 2, dataset)
+
+
+
+print('Test Accuracy of the model test images: {} %'.format(100 * correct / total))
