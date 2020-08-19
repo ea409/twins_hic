b1e72b9f4c755b3b13c37b71ab7ceb7db3c93139
ealjibur <e.aljibury@lms.mrc.ac.uk>
2020-08-19 18:23:25 +0100

    fix cuda

--- siamese_test.py
+++ siamese_test.py
@@ -64,7 +64,7 @@ rng = np.arange(mn, mx, (mx-mn)/200)
 
 a = plt.hist(distances[(labels==0)],bins=rng, density=True, label='technical replicates')
 b = plt.hist(distances[(labels==1)],bins=rng, density=True, label='biological conditions')
-intersect = a[1][np.argwhere(np.diff(np.sign(a[0]-b[0])))[0]]
+intersect = a[0][np.argwhere(np.diff(np.sign(a[0]-b[0])))[0]]
 plt.axvline(intersect, color='k')
 plt.xticks(np.arange(0,np.ceil(mx), 1))
 plt.legend()
