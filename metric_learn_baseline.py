41060bd7dcf6d13556be0eed7e73d5a5330a70cb
ealjibur <e.aljibury@lms.mrc.ac.uk>
2020-10-19 15:16:29 +0100

    tidying script

--- metric_learn_baseline.py
+++ metric_learn_baseline.py
@@ -1,16 +1,32 @@
-from HiCDataset import metriclearnpaired_HiCDataset
+from HiCDataset import metriclearnpaired_HiCDataset, HiCDatasetDec
 from metric_learn import SDML
 import numpy as np
 import pickle
+import argparse
+from reference_dictionaries import reference_genomes
+import json
 
+parser = argparse.ArgumentParser(description='metric learning baseline')
+parser.add_argument('json_file',  type=str,
+                    help='a file location for the json dictionary containing file paths')
+parser.add_argument('outfile',  type=str,
+                    help='a path for the output file')
+parser.add_argument("data_name", type=str,help="key from dictionary containing paths for training and validation sets.")
 
-mt = metriclearnpaired_HiCDataset(Siamese_validation, reference = reference_genomes[dataset[data_name]["reference"]], sims = [1,-1], resolution=880000, stride=4)
+args = parser.parse_args()
+
+with open(args.json_file) as json_file:
+    dataset = json.load(json_file)
+
+data = [HiCDatasetDec.load(data_path) for data_path in dataset[args.data_name]["training"]]
+
+mt = metriclearnpaired_HiCDataset(data, reference = reference_genomes[dataset[args.data_name]["reference"]], sims = [1,-1], resolution=880000, stride=4)
 x= np.array(mt)
 y= np.array(mt.labels)
 
 sdml = SDML()
 sdml.fit(x, y)
 
-with open("sdml_trained", 'wb') as output:
+with open(args.outfile, 'wb') as output:
     output.write(pickle.dumps(sdml))
     output.close()
