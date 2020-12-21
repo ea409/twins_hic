a177fc2c06459178f659d51fa24751748bcdb730
ealjibur <e.aljibury@lms.mrc.ac.uk>
2020-12-21 12:51:12 +0000

    attempting setup.py file with dep

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ version='0.0',
 author="Ediem Al-jibury",
 author_email="ealjibur@ic.ac.uk",
 description="HiSiNet: a tool for Hi-C analysis",
-install_requires=['cooler', 'frozendict','scipy==1.5.2', 'torch','numpy','hic-straw'], 
+install_requires=['cooler', 'frozendict','scipy==1.5.2', 'torch','numpy==1.18.0','hic-straw'], 
 url="https://gitlab.doc.ic.ac.uk/ealjibur/CNN",
 python_requires='>=3.6'
 )
