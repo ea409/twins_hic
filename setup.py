49ac2ae99293372939a46c55d99500ff329771d7
ea409 <33399273+ea409@users.noreply.github.com>
2022-12-16 16:48:16 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ author_email="ealjibur@ic.ac.uk",
 description="HiSiNet: a tool for Hi-C analysis",
 packages=['HiSiNet'],
 install_requires=['cooler==0.8.10', 'frozendict==1.2','scipy>=1.5.2', 'torch==1.6.0','numpy>=1.18.0', 'Cython==0.29.21', 
-                 'hic-straw @ git+git://github.com/aidenlab/straw.git' ], 
+                 'hic-straw @ git+git://github.com/aidenlab/straw.git@v0.0.8' ], 
 setup_requires=['cooler==0.8.10', 'frozendict==1.2','scipy>=1.5.2', 'torch==1.6.0','numpy>=1.18.0', 'Cython==0.29.21', 
                  'hic-straw==0.0.8'],
 python_requires='>=3.6'
