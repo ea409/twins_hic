375def3e3ea269eb7befbfdf376ef33b0e1399ce
ea409 <33399273+ea409@users.noreply.github.com>
2022-12-16 16:33:02 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ author="Ediem Al-jibury",
 author_email="ealjibur@ic.ac.uk",
 description="HiSiNet: a tool for Hi-C analysis",
 packages=['HiSiNet'],
-install_requires=['cooler==0.8.10', 'frozendict==1.2','scipy>=1.5.2', 'torch==1.6.0','numpy>=1.18.0', 'Cython==0.29.21'], 
-url="https://gitlab.doc.ic.ac.uk/ealjibur/CNN",
+install_requires=['cooler==0.8.10', 'frozendict==1.2','scipy>=1.5.2', 'torch==1.6.0','numpy>=1.18.0', 'Cython==0.29.21', 
+                 'hic-straw==0.0.8' ], 
+setup_requires=['cooler==0.8.10', 'frozendict==1.2','scipy>=1.5.2', 'torch==1.6.0','numpy>=1.18.0', 'Cython==0.29.21', 
+                 "straw @ git@github.com:aidenlab/straw.git@v0.0.8" ],
 python_requires='>=3.6'
 )
-
-#'hic-straw==0.0.8',
