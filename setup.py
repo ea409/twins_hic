8527c8b96bc5923df2cab6420400ba16dde24486
ea409 <33399273+ea409@users.noreply.github.com>
2022-12-16 16:43:28 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ packages=['HiSiNet'],
 install_requires=['cooler==0.8.10', 'frozendict==1.2','scipy>=1.5.2', 'torch==1.6.0','numpy>=1.18.0', 'Cython==0.29.21', 
                  'hic-straw==0.0.8' ], 
 setup_requires=['cooler==0.8.10', 'frozendict==1.2','scipy>=1.5.2', 'torch==1.6.0','numpy>=1.18.0', 'Cython==0.29.21', 
-                 "straw @ git@github.com:aidenlab/straw.git@v0.0.8" ],
+                 'hic-straw==0.0.8'],
 python_requires='>=3.6'
 )
